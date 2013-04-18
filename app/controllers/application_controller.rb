247e766542af68c5523fbe53dac237352d638f85
Sören Link (Macbook) <linksoeren@googlemail.com>
2013-04-18 21:32:24 +0200

    Added cancan redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   load_and_authorize_resource unless: :devise_controller?
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to new_user_registration_path, :alert => exception.message
+  end
 end
