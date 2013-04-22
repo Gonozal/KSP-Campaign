95a7f5e8f639d0c5f40083dea60b64dffe4b35cf
Arne Link <mail@arne-link.de>
2013-04-22 21:40:34 +0200

    fixed some deploy stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,6 +13,8 @@ set :runner, "deploy"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :app_server, :puma
 set :domain, "ksp-campaign.arnelink.eu"
+set :normalize_asset_timestamps, false
+default_run_options[:pty]   = true
 #========================
 #ROLES
 #========================
@@ -25,26 +27,27 @@ role :db, domain, :primary => true
 namespace :puma do
   desc "Start Puma"
   task :start, :except => { :no_release => true } do
-    run "sudo /etc/init.d/puma start #{application}"
+    run "/etc/init.d/puma start #{application}"
   end
   after "deploy:start", "puma:start"
 
   desc "Stop Puma"
   task :stop, :except => { :no_release => true } do
-    run "sudo /etc/init.d/puma stop #{application}"
+    run "/etc/init.d/puma stop #{application}"
   end
   after "deploy:stop", "puma:stop"
 
   desc "Restart Puma"
   task :restart, roles: :app do
-    run "sudo /etc/init.d/puma restart #{application}"
+    run "/etc/init.d/puma restart #{application}"
   end
   after "deploy:restart", "puma:restart"
 
   desc "create a shared tmp dir for puma state files"
   task :after_symlink, roles: :app do
-    run "sudo rm -rf #{release_path}/tmp"
+    run "rm -rf #{release_path}/tmp"
     run "ln -s #{shared_path}/tmp #{release_path}/tmp"
   end
   after "deploy:create_symlink", "puma:after_symlink"
 end
+
