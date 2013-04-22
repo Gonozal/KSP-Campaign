a0c80b80dbe5e8b82317785b9dc0b99a3b76e165
Sören Link (Macbook) <linksoeren@googlemail.com>
2013-04-22 19:54:26 +0200

    added deploy.rb

---
+++ config/deploy.rb
@@ -0,0 +1,50 @@
+#========================
+#CONFIG
+#========================
+set :application, "ksp_campaign"
+set :scm, :git
+set :repository, "git@github.com:Gonozal/KSP-Campaign.git"
+set :branch, "master"
+set :ssh_options, { :forward_agent => true }
+set :stage, :production
+set :user, "gonozal"
+set :use_sudo, false
+set :runner, "deploy"
+set :deploy_to, "/apps/#{application}"
+set :app_server, :puma
+set :domain, "ksp-campaign.arnelink.eu"
+#========================
+#ROLES
+#========================
+role :app, domain
+role :web, domain
+role :db, domain, :primary => true
+#========================
+#CUSTOM
+#========================
+namespace :puma do
+  desc "Start Puma"
+  task :start, :except => { :no_release => true } do
+    run "sudo /etc/init.d/puma start #{application}"
+  end
+  after "deploy:start", "puma:start"
+
+  desc "Stop Puma"
+  task :stop, :except => { :no_release => true } do
+    run "sudo /etc/init.d/puma stop #{application}"
+  end
+  after "deploy:stop", "puma:stop"
+
+  desc "Restart Puma"
+  task :restart, roles: :app do
+    run "sudo /etc/init.d/puma restart #{application}"
+  end
+  after "deploy:restart", "puma:restart"
+
+  desc "create a shared tmp dir for puma state files"
+  task :after_symlink, roles: :app do
+    run "sudo rm -rf #{release_path}/tmp"
+    run "ln -s #{shared_path}/tmp #{release_path}/tmp"
+  end
+  after "deploy:create_symlink", "puma:after_symlink"
+end
