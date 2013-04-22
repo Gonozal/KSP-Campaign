7d83e1578008c65d81c234ce665855af09433c0d
Sören Link (Macbook) <linksoeren@googlemail.com>
2013-04-22 22:53:23 +0200

    heroku deploy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KspCampaign::Application.config.secret_key_base = 'ff6f86338337bda971483758e2ac26d1c3ddbc675c3db6f6c1b4c4cde590af188a3470f0cc85f76c109cabbc4e16375b49708be079d3f081e7e5f7bf61d90040'
